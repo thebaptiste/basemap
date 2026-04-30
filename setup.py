2d504cfde10af5b32eff87cd20e4b40ae30beb0e
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2026-04-30 18:44:53 +0200

    build: test for compatibility Python 3.14

--- setup.py
+++ setup.py
@@ -109,10 +109,7 @@ else:
     try:
         import numpy
         include_dirs.append(numpy.get_include())
-    except ImportError as err:
-        cmds = ("bdist_wheel", "build", "install")
-        if any(cmd in sys.argv[1:] for cmd in cmds):
-            warnings.warn("unable to locate NumPy headers", RuntimeWarning)
+        warnings.warn("NumPy headers", numpy.get_include())
 
 # Define GEOS include, library and runtime dirs.
 geos_install_prefix = get_geos_install_prefix()
